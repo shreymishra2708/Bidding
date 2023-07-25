da95eff366e2c16b86acaf0b7154542cb5c7009a
shreymishra2708 <shreymishra128@gmail.com>
2023-07-25 11:38:36 +0530

    initial

--- abc.js
+++ abc.js
@@ -1,42 +1,42 @@
-$(function(){
+// $(function(){
   
   
-    loop();
+//     loop();
       
       
-    });
+//     });
     
     
-    function randomIntFromInterval(min, max) { 
-      return Math.floor(Math.random() * (max - min + 1) + min)
-    }
+//     function randomIntFromInterval(min, max) { 
+//       return Math.floor(Math.random() * (max - min + 1) + min)
+//     }
     
-    function loop(){
+//     function loop(){
         
-      var randomPercent = randomIntFromInterval(1, 6);
-      var randomTime = randomIntFromInterval(5, 10) * 1000;
-      var factor =  1 - (randomPercent/100);
-      var oldVal = parseFloat(document.getElementById("mainElement").innerHTML.trim());
+//       var randomPercent = randomIntFromInterval(1, 6);
+//       var randomTime = randomIntFromInterval(5, 10) * 1000;
+//       var factor =  1 - (randomPercent/100);
+//       var oldVal = parseFloat(document.getElementById("mainElement").innerHTML.trim());
       
-      var newVal = (factor * oldVal).toFixed(2);
-      console.log({oldVal,randomPercent, randomTime, factor,newVal});
+//       var newVal = (factor * oldVal).toFixed(2);
+//       console.log({oldVal,randomPercent, randomTime, factor,newVal});
     
-      $("#mainElement").html(newVal);
+//       $("#mainElement").html(newVal);
       
-      setTimeout(function(){
-        loop();
-      },randomTime)
+//       setTimeout(function(){
+//         loop();
+//       },randomTime)
       
-    }
+//     }
     
     
-    function loop1(){
+//     function loop1(){
       
-      // detect changes in element #mainElement
-      // new current value
-      // decrement current value
-      // newVal >= Stop loss
-      // proceed
+//       // detect changes in element #mainElement
+//       // new current value
+//       // decrement current value
+//       // newVal >= Stop loss
+//       // proceed
       
       
-    }
+//     }
